091b239af6e9de89282eb28a370180cf80e2c9f9
Prasant Jha <prasantjha4444@gmail.com>
2025-02-06 20:09:23 +0530

    Updated project contents

--- Jobby/src/pages/Home.jsx
+++ Jobby/src/pages/Home.jsx
@@ -14,14 +14,14 @@ const Home = () => {
 
       {/* Main Content */}
       <div className="flex flex-wrap items-center justify-evenly min-h-[92vh] bg-gradient-to-r from-blue-500 to-indigo-600">
-        <div className="flex flex-col justify-center space-y-6 text-white max-w-[40%]">
+        <div className="flex flex-col justify-center space-y-6 text-white max-w-[50%]">
           <h4 className="text-[36px] font-semibold sm:w-3/4 lg:w-2/3 xl:w-3/4">
             Explore our site for a better experience with job searching
           </h4>
           <p className="text-lg sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-2/3">
             You will surely find your ideal job choice with ease.
           </p>
-          <button className="h-[45px] w-[180px] sm:w-[220px] bg-blue-600 text-white text-lg font-semibold rounded-md hover:bg-blue-700 transition duration-300" onClick={() => navigate("/login")}>
+          <button className="h-[45px] w-[180px] sm:w-[220px] bg-blue-600 border-2 text-white text-lg font-semibold rounded-md hover:bg-blue-700 transition duration-300" onClick={() => navigate("/login")}>
             Get Started
           </button>
         </div>
@@ -29,7 +29,7 @@ const Home = () => {
           <img
             src={JobImage}
             alt="Job search"
-            className="w-full h-auto object-cover rounded-lg shadow-lg transform transition-transform duration-300 hover:scale-105"
+            className="w-[80%] h-auto object-cover rounded-lg shadow-lg transform transition-transform duration-300 hover:scale-105"
           />
         </div>
       </div>
